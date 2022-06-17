3fc9f84552cc298b02815157fa94760a4196ac92
marcelobellotto <marcelo.bellotto@asaptechnology.com.ar>
2022-06-16 23:11:26 -0300

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,41 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, OnInit, VERSION } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   name = 'Ionic 6 Angular ' + VERSION.major;
+
+  spinnerActive = false;
+  actionStarted = false;
+  actionCompleted = false;
+
+  ngOnInit() {
+    // present the action
+    this.actionStarted = true;
+    setTimeout( () => {
+      this.startAction();
+    }, 400); 
+  }
 
   ionViewDidEnter() {
     console.log('CRAP')
+  }
+
+  startAction() {
+    this.spinnerActive = true;
+    setTimeout( () => {
+      this.updater();
+    }, 1500);
+  }
+
+  updater() {
+    setTimeout( () => {
+      this.spinnerActive = false;
+      this.actionStarted = false; 
+      this.actionCompleted = true;
+    }, 2000);
   }
 }
